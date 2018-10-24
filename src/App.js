6309aac920dc739c15ab50198b77bfa7c773db47
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-24 11:07:05 +0900

    1 split logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { Logo } from "./Logo"
+
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Logo />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -21,8 +22,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Logo.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import logo from "./logo.svg"
+
+export const Logo = () => {
+  return <img src={logo} className="App-logo" alt="logo" />
+}
