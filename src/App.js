2571473dad18616a993f1428f4ccb4072c7f736a
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-24 11:13:06 +0900

    non named

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react"
-import { Logo } from "./Logo"
-
+import React, { Component, lazy, Suspense } from "react"
 import "./App.css"
+const Logo = lazy(() => import("./Logo"))
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Logo />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Logo />
+          </Suspense>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
--- src/Logo.js
+++ src/Logo.js
@@ -2,6 +2,6 @@ import React from "react"
 
 import logo from "./logo.svg"
 
-export const Logo = () => {
+export default () => {
   return <img src={logo} className="App-logo" alt="logo" />
 }
